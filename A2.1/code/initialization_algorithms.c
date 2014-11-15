92ec6780de99496a57cbe819b15191046d01dcaf
Denys S <denys.sobchyshak@gmail.com>
2014-11-15 22:19:52 +0100

    added prototype of allread

--- A2.1/code/initialization_algorithms.c
+++ A2.1/code/initialization_algorithms.c
@@ -27,6 +27,7 @@ int read_global_data_or_geometry(char* file_in,char* read_type, int myrank,
 		double **bl, double **bh, double **bp, double **su,
 		int* points_count, int***points, int** elems) {
 	int f_status=0;
+    //TODO:change the read function to read only the geometry data
 	if ( !strcmp( read_type, "oneread" ) ) {
 		if(myrank == 0) {
 			// read-in the input file
@@ -125,13 +126,90 @@ int compute_metis(char* part_type, char* read_type, int myrank, int nprocs,
             //if our domain can't be divided in equal parts (breakets are very important!!!)
 			extcell_per_proc[nprocs-1] = (nextcf_g - nextci_g + 1)
                     - (nprocs-1)*((nextcf_g - nextci_g + 1 + (nprocs-1))/nprocs);
-			fill_local_global_index(nprocs,*local_global_index_g, ne, *metis_idx,intcell_per_proc);
+			fill_local_global_index(nprocs,*local_global_index_g, ne, *metis_idx, intcell_per_proc);
 		}
 		count_ext_cells(nprocs, *local_global_index_g,
 				nintci_g, nintcf_g, nextci_g, nextcf_g,
 				lcc_g, *metis_idx,
 				intcell_per_proc, extcell_per_proc);
-	}
+	} else {
+
+        // Allocate
+        if ( (*local_global_index_g = (int *) malloc((nintcf_g + 1) * sizeof(int))) == NULL ) {
+            fprintf(stderr, "malloc(local_global_index) failed\n");
+            return -1;
+        }
+
+        *metis_idx = ( int* ) calloc( sizeof( int ), ( nintcf_g - nintci_g + 1 ) );
+
+        if (!strcmp( part_type, "classic" )) {
+            for (i=0; i<nprocs-1; i++) {
+                // (nprocs-1) is important for not to loosing one cells in the processes
+                intcell_per_proc[i] = (nintcf_g + 1 + (nprocs-1))/nprocs;
+            }
+            //if our domain can't be divided in equal parts (breakets are very important!!!)
+            intcell_per_proc[nprocs-1] = nintcf_g + 1 - (nprocs-1)*((nintcf_g + 1 + (nprocs-1))/nprocs);
+            for (i=0;i<(nintcf_g + 1);i++) {
+                (*local_global_index_g)[i] = i;
+            }
+            for (proc=0; proc<nprocs; ++proc) {
+                for(i=0; i<intcell_per_proc[proc]; ++i) {
+                    (*metis_idx)[start_idx+i] = proc;
+                }
+                start_idx += intcell_per_proc[proc];
+            }
+        } else {
+            ne = nintcf_g - nintci_g + 1;
+            nn = points_count_g;
+            ncommon = 4;
+            nparts = nprocs;
+
+            eptr = ( idx_t* ) calloc( sizeof( idx_t ), ( ne + 1 ) );
+            eind = ( idx_t* ) calloc( sizeof( idx_t ), ( ne * 8 ) );
+// TODO:		    npart = ( int* ) calloc( sizeof( int ), ( nn ) );
+            epart_idx = ( idx_t* ) calloc( sizeof( idx_t ), ( ne ) );
+            npart_idx = ( idx_t* ) calloc( sizeof( idx_t ), ( nn ) );
+
+            for( i = 0; i < ( ne + 1 ); i++ ) {
+                eptr[i] = 8 * i;
+            }
+            for( i = 0; i < ( ne * 8 ); i++ ) {
+                eind[i] = elems_g[i];
+            }
+            if ( !strcmp( part_type, "classic" ) ) {
+                METIS_PartMeshDual(&ne, &nn, eptr, eind, NULL, NULL, &ncommon, &nparts, NULL, NULL, &objval_idx,
+                        epart_idx, npart_idx);
+            } else {
+                METIS_PartMeshNodal(&ne, &nn, eptr, eind, NULL, NULL, &nparts, NULL, NULL, &objval_idx,
+                        epart_idx, npart_idx);
+            }
+            for(i = 0; i < ne; i++) {
+                (*metis_idx)[i] = ( int )epart_idx[i];
+            }
+//	        // TODO: delete
+//		    for(i=0;i<20;i++) {
+//		    	printf("%d !!!!!!!%d\n",i, (*metis_idx)[i]);
+//		    }
+            for(i=0; i<nprocs; i++) {
+                intcell_per_proc[i] = 0;
+            }
+//			// TODO: mover extcell_per_proc from here and from classic distr
+//			for(i=0; i<nprocs-1; i++) {
+//				extcell_per_proc[i] = (nextcf_g - nextci_g + 1 + (nprocs-1))/nprocs;
+//			}
+            //if our domain can't be divided in equal parts (breakets are very important!!!)
+            extcell_per_proc[nprocs-1] = (nextcf_g - nextci_g + 1)
+                    - (nprocs-1)*((nextcf_g - nextci_g + 1 + (nprocs-1))/nprocs);
+            fill_local_global_index(nprocs,*local_global_index_g, ne, *metis_idx,intcell_per_proc);
+        }
+        count_ext_cells(nprocs, *local_global_index_g,
+                nintci_g, nintcf_g, nextci_g, nextcf_g,
+                lcc_g, *metis_idx,
+                intcell_per_proc, extcell_per_proc);
+
+
+
+    }
 	return 0;
 }
 
@@ -167,7 +245,14 @@ int allocate_local_variables(char* read_type, int myrank, int nprocs,
 			*nintcf = *nintcf-1;	// We need to subtract, because we got the number of cells(not the last index!)
 			*nextcf = *nintcf + *nextcf;	// We need to subtract, because we got the number of cells(not the last index!)
 		}
-	}
+	} else {
+        //TOREVIEW:check this with Denys
+        *nintci = 0;
+        *nintcf = intcell_per_proc[myrank]-1;
+        *nextci = intcell_per_proc[myrank];
+        *nextcf = *nintcf + extcell_per_proc[myrank];
+        *points_count = points_count_g;
+    }
 	// Allocation
     if ( (*lcc = (int**) malloc((*nintcf + 1) * sizeof(int*))) == NULL ) {
         fprintf(stderr, "malloc failed to allocate first dimension of LCC");
@@ -238,7 +323,7 @@ int allocate_local_variables(char* read_type, int myrank, int nprocs,
         }
     }
     // End allocation
-return 0;
+    return 0;
 }
 
 
@@ -256,12 +341,10 @@ int send_or_read_data(char* read_type, int myrank, int nprocs,
 		double **bl_g, double **bh_g, double **bp_g, double **su_g,
 		int points_count_g, int** points_g, int **elems_g,
 		int *local_global_index_g) {
-	int k = 0;
-	int i=0;
-	MPI_Status status ;
-	if( !strcmp( read_type, "oneread" ) ) {
-		if(myrank==0) {
-
+	int k=0, i=0;
+	MPI_Status status;
+	if(!strcmp( read_type, "oneread" )) {
+		if(myrank == 0) {
 			// TODO: check on bad allocation
 			sort_data_by_local_global_index(nintci_g, nintcf_g, nextci_g, nextcf_g,
 					&*bs_g, &*be_g, &*bn_g, &*bw_g,
@@ -269,7 +352,8 @@ int send_or_read_data(char* read_type, int myrank, int nprocs,
 					&*elems_g,
 					local_global_index_g);
 
-			int start_idx = 0;	// Used to send data, from some index(because nodes can have not equal number of cells)
+            // Used to send data, from some index(because nodes can have not equal number of cells)
+			int start_idx = 0;
 			// Copy memory for process 0
 			for(i=nintci; i<nintcf + 1; i++) {
 				memcpy(lcc[i], lcc_g[local_global_index_g[i]], 6*sizeof(int));
@@ -286,70 +370,77 @@ int send_or_read_data(char* read_type, int myrank, int nprocs,
 				memcpy(points[i], points_g[i], 3*sizeof(int));
 			}
 			memcpy(elems, *elems_g, intcell_per_proc[0]*8*sizeof(int));
-			memcpy(local_global_index, local_global_index_g,
-					intcell_per_proc[0] * sizeof(int));
+			memcpy(local_global_index, local_global_index_g, intcell_per_proc[0] * sizeof(int));
 			// Send all other data
 			for (k=1; k<nprocs; ++k) {
 				start_idx += intcell_per_proc[k-1];
 				for(i=0; i < intcell_per_proc[k]; ++i) {
-					MPI_Send(lcc_g[local_global_index_g[start_idx + i]],6,MPI_INT,k,
-							0,MPI_COMM_WORLD);
+					MPI_Send(lcc_g[local_global_index_g[start_idx + i]], 6, MPI_INT, k, 0, MPI_COMM_WORLD);
 				}
-				MPI_Send(&(*bs_g)[start_idx],intcell_per_proc[k],
-						MPI_DOUBLE, k, 1, MPI_COMM_WORLD);
-				MPI_Send(&(*be_g)[start_idx],intcell_per_proc[k],
-						MPI_DOUBLE, k, 2, MPI_COMM_WORLD);
-				MPI_Send(&(*bn_g)[start_idx],intcell_per_proc[k],
-						MPI_DOUBLE, k, 3, MPI_COMM_WORLD);
-				MPI_Send(&(*bw_g)[start_idx],intcell_per_proc[k],
-						MPI_DOUBLE, k, 4, MPI_COMM_WORLD);
-				MPI_Send(&(*bl_g)[start_idx],intcell_per_proc[k],
-						MPI_DOUBLE, k, 5, MPI_COMM_WORLD);
-				MPI_Send(&(*bh_g)[start_idx],intcell_per_proc[k],
-						MPI_DOUBLE, k, 6, MPI_COMM_WORLD);
-				MPI_Send(&(*bp_g)[start_idx],intcell_per_proc[k],
-						MPI_DOUBLE, k, 7, MPI_COMM_WORLD);
-				MPI_Send(&(*su_g)[start_idx],intcell_per_proc[k],
-						MPI_DOUBLE, k, 8, MPI_COMM_WORLD);
+				MPI_Send(&(*bs_g)[start_idx], intcell_per_proc[k], MPI_DOUBLE, k, 1, MPI_COMM_WORLD);
+				MPI_Send(&(*be_g)[start_idx], intcell_per_proc[k], MPI_DOUBLE, k, 2, MPI_COMM_WORLD);
+				MPI_Send(&(*bn_g)[start_idx], intcell_per_proc[k], MPI_DOUBLE, k, 3, MPI_COMM_WORLD);
+				MPI_Send(&(*bw_g)[start_idx], intcell_per_proc[k], MPI_DOUBLE, k, 4, MPI_COMM_WORLD);
+				MPI_Send(&(*bl_g)[start_idx], intcell_per_proc[k], MPI_DOUBLE, k, 5, MPI_COMM_WORLD);
+				MPI_Send(&(*bh_g)[start_idx], intcell_per_proc[k], MPI_DOUBLE, k, 6, MPI_COMM_WORLD);
+				MPI_Send(&(*bp_g)[start_idx], intcell_per_proc[k], MPI_DOUBLE, k, 7, MPI_COMM_WORLD);
+				MPI_Send(&(*su_g)[start_idx], intcell_per_proc[k], MPI_DOUBLE, k, 8, MPI_COMM_WORLD);
 				for ( i = 0; i < points_count; i++ ) {
-					MPI_Send(points_g[i],3,MPI_INT,k,99,MPI_COMM_WORLD);
+					MPI_Send(points_g[i], 3, MPI_INT, k, 99, MPI_COMM_WORLD);
 				}
-				MPI_Send(&(*elems_g)[start_idx*8],intcell_per_proc[k]*8,
-						MPI_INT, k, 100, MPI_COMM_WORLD);
-				MPI_Send(&local_global_index_g[start_idx],
-						intcell_per_proc[k], MPI_INT, k, 101, MPI_COMM_WORLD);
-
+				MPI_Send(&(*elems_g)[start_idx*8], intcell_per_proc[k]*8, MPI_INT, k, 100, MPI_COMM_WORLD);
+				MPI_Send(&local_global_index_g[start_idx], intcell_per_proc[k], MPI_INT, k, 101, MPI_COMM_WORLD);
 			}
 		} else {
 			// Receive data from process 0
 			for(i=nintci; i < nintcf; ++i) {
-				MPI_Recv(lcc[i],6, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+				MPI_Recv(lcc[i], 6, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 			}
-			MPI_Recv(bs,(nintcf-nintci+1),
-					MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
-			MPI_Recv(be,(nintcf-nintci+1),
-					MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, &status);
-			MPI_Recv(bn,(nintcf-nintci+1),
-					MPI_DOUBLE, 0, 3, MPI_COMM_WORLD, &status);
-			MPI_Recv(bw,(nintcf-nintci+1),
-					MPI_DOUBLE, 0, 4, MPI_COMM_WORLD, &status);
-			MPI_Recv(bl,(nintcf-nintci+1),
-					MPI_DOUBLE, 0, 5, MPI_COMM_WORLD, &status);
-			MPI_Recv(bh,(nintcf-nintci+1),
-					MPI_DOUBLE, 0, 6, MPI_COMM_WORLD, &status);
-			MPI_Recv(bp,(nintcf-nintci+1),
-					MPI_DOUBLE, 0, 7, MPI_COMM_WORLD, &status);
-			MPI_Recv(su,(nintcf-nintci+1),
-					MPI_DOUBLE, 0, 8, MPI_COMM_WORLD, &status);
+			MPI_Recv(bs, (nintcf-nintci+1), MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
+			MPI_Recv(be, (nintcf-nintci+1), MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, &status);
+			MPI_Recv(bn, (nintcf-nintci+1), MPI_DOUBLE, 0, 3, MPI_COMM_WORLD, &status);
+			MPI_Recv(bw, (nintcf-nintci+1), MPI_DOUBLE, 0, 4, MPI_COMM_WORLD, &status);
+			MPI_Recv(bl, (nintcf-nintci+1), MPI_DOUBLE, 0, 5, MPI_COMM_WORLD, &status);
+			MPI_Recv(bh, (nintcf-nintci+1), MPI_DOUBLE, 0, 6, MPI_COMM_WORLD, &status);
+			MPI_Recv(bp, (nintcf-nintci+1), MPI_DOUBLE, 0, 7, MPI_COMM_WORLD, &status);
+			MPI_Recv(su, (nintcf-nintci+1), MPI_DOUBLE, 0, 8, MPI_COMM_WORLD, &status);
 			for ( i = 0; i < points_count; i++ ) {
-				MPI_Recv(points[i],3, MPI_INT, 0, 99, MPI_COMM_WORLD, &status);
+				MPI_Recv(points[i], 3, MPI_INT, 0, 99, MPI_COMM_WORLD, &status);
 			}
-			MPI_Recv(elems,(nintcf-nintci+1)*8,
-					MPI_INT, 0, 100, MPI_COMM_WORLD, &status);
-			MPI_Recv(local_global_index,(nintcf-nintci+1),
-					MPI_INT, 0, 101, MPI_COMM_WORLD, &status);
+			MPI_Recv(elems, (nintcf-nintci+1)*8, MPI_INT, 0, 100, MPI_COMM_WORLD, &status);
+			MPI_Recv(local_global_index, (nintcf-nintci+1), MPI_INT, 0, 101, MPI_COMM_WORLD, &status);
 		}
-	}
+	} else {
+        // TODO: check on bad allocation
+        sort_data_by_local_global_index(nintci_g, nintcf_g, nextci_g, nextcf_g,
+                &*bs_g, &*be_g, &*bn_g, &*bw_g,
+                &*bl_g, &*bh_g, &*bp_g, &*su_g,
+                &*elems_g,
+                local_global_index_g);
+
+        // Used to send data, from some index(because nodes can have not equal number of cells)
+        int start_idx = 0;
+        for (k=0; k<(myrank-1); ++k) {
+            start_idx += intcell_per_proc[k];
+        }
+        // Copy memory for process 0
+        for(i=nintci; i<nintcf + 1; i++) {
+            memcpy(lcc[i], lcc_g[local_global_index_g[start_idx+i]], 6*sizeof(int));
+        }
+        memcpy(bs, &(*bs_g)[start_idx], intcell_per_proc[0] * sizeof(double));
+        memcpy(be, &(*be_g)[start_idx], intcell_per_proc[0] * sizeof(double));
+        memcpy(bn, &(*bn_g)[start_idx], intcell_per_proc[0] * sizeof(double));
+        memcpy(bw, &(*bw_g)[start_idx], intcell_per_proc[0] * sizeof(double));
+        memcpy(bl, &(*bl_g)[start_idx], intcell_per_proc[0] * sizeof(double));
+        memcpy(bh, &(*bh_g)[start_idx], intcell_per_proc[0] * sizeof(double));
+        memcpy(bp, &(*bp_g)[start_idx], intcell_per_proc[0] * sizeof(double));
+        memcpy(su, &(*su_g)[start_idx], intcell_per_proc[0] * sizeof(double));
+        for ( i = 0; i < points_count; i++ ) {
+            memcpy(points[i], points_g[i], 3*sizeof(int));
+        }
+        memcpy(elems, *elems_g, intcell_per_proc[myrank]*8*sizeof(int));
+        memcpy(local_global_index, local_global_index_g, intcell_per_proc[myrank]*sizeof(int));
+    }
 	return 0;
 }
 
@@ -381,13 +472,11 @@ int sort_data_by_local_global_index(int nintci_g, int nintcf_g, int nextci_g, in
 	int *tmp_elems;	// This two arrays are needed to send data in correct order
 	double *tmp_b;
 	void *tmp; // Not to lose the pointer
-	if ((tmp_elems = (int *) malloc((nintcf_g+1)*8 * sizeof(int))) == NULL)
-	{
+	if ((tmp_elems = (int *) malloc((nintcf_g+1)*8*sizeof(int))) == NULL) {
 		printf("malloc() of tmp_elems in send_or_read_data failed\n");
 		return -1;
 	}
-	if ((tmp_b = (double *) malloc((nintcf_g + 1) * sizeof(double))) == NULL)
-	{
+	if ((tmp_b = (double *) malloc((nintcf_g+1)*sizeof(double))) == NULL) {
 		printf("malloc() of tmp_b in send_or_read_data failed\n");
 		return -1;
 	}

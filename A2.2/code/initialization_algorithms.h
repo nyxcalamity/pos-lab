c6b4087953fb9d6bf35f76559b3bcc2c537cad08
Denys S <denys.sobchyshak@gmail.com>
2014-11-23 18:17:00 +0100

    improved code organisation

--- A2.2/code/initialization_algorithms.h
+++ A2.2/code/initialization_algorithms.h
@@ -1,149 +1,62 @@
-/*
- * initialization_algorithms.h
- *
- *  Created on: Nov 10, 2014
- *      Author: power-morzh
- */
-
 #ifndef INITIALIZATION_ALGORITHMS_H_
 #define INITIALIZATION_ALGORITHMS_H_
 
-/**
- * Reads global data if we have "oneread" read type or
- * reads data needed by METIS computation if we have "allread"
- *
- * @param file_name
- * @param read_type
- * @param myrank
- * @param nintci
- * @param nintcf
- * @param nextci
- * @param nextcf
- * @param lcc
- * @param BS
- * @param BE
- * @param BN
- * @param BW
- * @param BL
- * @param BH
- * @param BP
- * @param SU
- * @param points_count
- * @param points
- * @param elems
- * @return
- */
-int read_global_data_or_geometry(char* file_in,char* read_type, int myrank,
-        int *nintci, int *nintcf, int *nextci, int *nextcf,
-        int ***lcc,
-        double **bs, double **be, double **bn, double **bw,
-		double **bl, double **bh, double **bp, double **su,
-		int* points_count, int***points, int** elems);
 
 /**
- * Uses read geometry data to allocate and compute local_global_index and
- * to fill cell_per_node
- *
- * @param file_name
- * @param read_type
- * @param myrank
- * @param nprocs
- * @param nintci_g
- * @param nintcf_g
- * @param nextci_g
- * @param nextcf_g
- * @param nintci
- * @param nintcf
- * @param nextci
- * @param nextcf
- * @param lcc
- * @param points_count
- * @param points
- * @param elems
- * @param intcell_per_proc
- * @param extcell_per_proc
- * @param local_global_index
- * @return
+ * Reads data needed for computation.
+ */
+int read_global_data_or_geometry(char* file_in,char* read_type, int myrank, int *nintci, int *nintcf, 
+        int *nextci, int *nextcf, int ***lcc, double **bs, double **be, double **bn, double **bw, 
+        double **bl, double **bh, double **bp, double **su, int* points_count, int***points, int** elems);
+
+
+/**
+ * Computes external and internal cells starting and ending positions.
  */
 int compute_metis(char* part_type, char* read_type, int myrank, int nprocs, int nintci_g,
         int nintcf_g, int nextci_g, int nextcf_g, int *nintci, int *nintcf, int *nextci, int *nextcf,
         int **lcc_g, int points_count_g, int**points_g, int* elems_g, int *intcell_per_proc,
         int *extcell_per_proc, int** local_global_index_g, int** local_global_index, int **metis_idx);
 
-/**
- * Send and receive or compute nintci,nintcf,nextci,nextcf and allocate needed
- * for computation arrays of needed size
- *
- * @param read_type
- * @param myrank
- * @param nprocs
- * @param nintci
- * @param nintcf
- * @param nextci
- * @param nextcf
- * @param lcc
- * @param points_count
- * @param points
- * @param elems
- * @param intcell_per_proc
- * @param extcell_per_proc
- * @param local_global_index
- * @return
- */
-int allocate_lcc_elems_points(char* read_type, int myrank, int nprocs, int *nintci, int *nintcf,
-       int ***lcc, int* points_count, int*** points,
-        int** elems, int **local_global_index, int points_count_g);
 
 /**
- * Send and receive or read all needed local data
- *
- * @param read_type
- * @param myrank
- * @param nprocs
- * @param nintci
- * @param nintcf
- * @param nextci
- * @param nextcf
- * @param lcc
- * @param bs
- * @param be
- * @param bn
- * @param bw
- * @param bl
- * @param bh
- * @param bp
- * @param su
- * @param points_count
- * @param points
- * @param elems
- * @param intcell_per_proc
- * @param extcell_per_proc
- * @param local_global_index
- * @return
+ * Exchange or find in global array nintci,nintcf,nextci,nextcf and allocate corresponding memory for
+ * elements, points and lcc.
  */
-int fill_lcc_elems_points(char* read_type, int myrank, int nprocs, int nintci, int nintcf,
-        int **lcc, int points_count, int** points, int* elems,
-        int *local_global_index,  int **lcc_g,
+int allocate_lcc_elems_points(char* read_type, int myrank, int nprocs, int *nintci, int *nintcf, 
+        int ***lcc, int* points_count, int*** points, int** elems, int **local_global_index, 
+        int points_count_g);
+
+
+/**
+ * Exchange or copy from global all local data.
+ */
+int fill_lcc_elems_points(char* read_type, int myrank, int nprocs, int nintci, int nintcf, int **lcc, 
+        int points_count, int** points, int* elems, int *local_global_index,  int **lcc_g, 
         int points_count_g, int** points_g, int **elems_g);
 
-int allocate_boundary_coef(char* read_type, int myrank, int nprocs, int *nextcf, double **bs, double **be, double **bn, double **bw,
-        double **bl, double **bh, double **bp, double **su);
+/**
+ * Allocates local memory for various boundary coefficients.
+ */
+int allocate_boundary_coef(char* read_type, int myrank, int nprocs, int *nextcf, double **bs, 
+        double **be, double **bn, double **bw, double **bl, double **bh, double **bp, double **su);
 
+
+/**
+ * Exchange local values of boundary coefficients or copy them from global values.
+ */
 int fill_boundary_coef(char* read_type, int myrank, int nprocs, int nintci, int nintcf, int nextci,
-        int nextcf, double *bs, double *be, double *bn, double *bw, double *bl,
-        double *bh, double *bp, double *su,
-        int *local_global_index, double **bs_g, double **be_g,
-        double **bn_g, double **bw_g, double **bl_g, double **bh_g, double **bp_g, double **su_g);
+        int nextcf, double *bs, double *be, double *bn, double *bw, double *bl,  double *bh, 
+        double *bp, double *su, int *local_global_index, double **bs_g, double **be_g, double **bn_g, 
+        double **bw_g, double **bl_g, double **bh_g, double **bp_g, double **su_g);
 
-void fill_local_global_index(char* read_type, int myrank, int nintci, int nintcf,
+
+/**
+ * Computes a 1D array with its indexes as local cell IDs and its values as global cell IDs.
+ */
+void fill_local_global_index(char* read_type, int myrank, int nintci, int nintcf, 
         int** local_global_index, int *metis_idx, int nelems_g);
 
-// TODO: comment
-int sort_data_by_local_global_index(int nintci_g, int nintcf_g, int nextci_g, int nextcf_g,
-	double **bs_g, double **be_g, double **bn_g, double **bw_g,
-	double **bl_g, double **bh_g, double **bp_g, double **su_g,
-	int **elems_g,
-	int *local_global_index_g);
 
 /**
  * Counts the number of externall cell for each process and change lcc to have

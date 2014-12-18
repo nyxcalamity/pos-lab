db52619616f672fe0961c6f5f63eb0e6e0195ae6
Denys S <denys.sobchyshak@gmail.com>
2014-12-18 18:54:23 +0100

    a2.3:added proper job file

--- A2.3/scripts/job.sh
+++ A2.3/scripts/job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#@ job_name = pos_ws22
+#@ job_name = pos_ws23
 #@ job_type = parallel
 #@ class = test
 #@ island_count=1
 #@ node = 1
 #@ total_tasks = 9
 #@ wall_clock_limit = 0:30:00
-#@ energy_policy_tag = pos_ws22_tag
+#@ energy_policy_tag = pos_ws23_tag
 
 #@ initialdir = $(home)/pos-lab/A2.3/code
 #@ output = $(home)/job.out
@@ -20,36 +20,5 @@
 perf_off
 
 ##execution
-echo "============================================================================================="
-echo "Statistics output"
-echo "============================================================================================="
-echo "============================================================================================="
-echo "drall output"
-echo "============================================================================================="
-mpiexec -n 9 ./gccg ./data/drall.geo.bin classic allread
 mpiexec -n 9 ./gccg ./data/drall.geo.bin classic oneread
-mpiexec -n 9 ./gccg ./data/drall.geo.bin dual allread
-mpiexec -n 9 ./gccg ./data/drall.geo.bin dual oneread
-mpiexec -n 9 ./gccg ./data/drall.geo.bin nodal allread
-mpiexec -n 9 ./gccg ./data/drall.geo.bin nodal oneread
-echo "============================================================================================="
-echo "pent output"
-echo "============================================================================================="
-mpiexec -n 9 ./gccg ./data/pent.geo.bin classic allread
-mpiexec -n 9 ./gccg ./data/pent.geo.bin classic oneread
-mpiexec -n 9 ./gccg ./data/pent.geo.bin dual allread
-mpiexec -n 9 ./gccg ./data/pent.geo.bin dual oneread
-mpiexec -n 9 ./gccg ./data/pent.geo.bin nodal allread
-mpiexec -n 9 ./gccg ./data/pent.geo.bin nodal oneread
-echo "============================================================================================="
-echo "cojack output"
-echo "============================================================================================="
-mpiexec -n 9 ./gccg ./data/cojack.geo.bin classic allread
-mpiexec -n 9 ./gccg ./data/cojack.geo.bin classic oneread
-mpiexec -n 9 ./gccg ./data/cojack.geo.bin dual allread
-mpiexec -n 9 ./gccg ./data/cojack.geo.bin dual oneread
-mpiexec -n 9 ./gccg ./data/cojack.geo.bin nodal allread
-mpiexec -n 9 ./gccg ./data/cojack.geo.bin nodal oneread
-echo "============================================================================================="
-echo "Finish output"
-echo "============================================================================================="
+

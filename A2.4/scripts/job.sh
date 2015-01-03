e4532cfb4ae3be43a4b294efad1212ae09c0d109
Denys S <denys.sobchyshak@gmail.com>
2015-01-03 14:24:38 +0100

    project structure cleanup

--- A2.4/scripts/job.sh
+++ A2.4/scripts/job.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#@ job_name = pos_ws23
+#@ job_name = pos_ws24
 #@ job_type = parallel
 #@ class = test
 #@ island_count=1
 #@ node = 1
 #@ total_tasks = 9
 #@ wall_clock_limit = 0:30:00
-#@ energy_policy_tag = pos_ws23_tag
+#@ energy_policy_tag = pos_ws24_tag
 
-#@ initialdir = $(home)/pos-lab/A2.3/code
-#@ output = $(home)/job.out
-#@ error = $(home)/job.err
+#@ initialdir = $(home)/pos-lab/A2.4
+#@ output = $(home)/job24.out
+#@ error = $(home)/job24.err
 
 #@ notification=always
-#@ notify_user=denys.korzh@tum.de
+#@ notify_user=denys.sobchyshak@tum.de
 #@ queue
 
 #system setup
 perf_off
 
-##execution
-mpiexec -n 9 ./gccg ./data/drall.geo.bin classic oneread
+#minimum optimization objective command
+mpiexec -n 9 ./optimization1/optimized/gccg ./code/data/pent.geo.bin nodal oneread
 

b645cd95caf7b68ece84d811228c7f99e7290fcc
Denys S <denys.sobchyshak@gmail.com>
2015-01-16 16:01:18 +0100

    a24:added new scripts for data collection

---
+++ A2.4/scripts/job-run-np7.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#@ job_name = pos_ws24
+#@ job_type = parallel
+#@ class = test
+#@ island_count=1
+#@ node = 1
+#@ total_tasks = 2
+#@ wall_clock_limit = 0:30:00
+#@ energy_policy_tag = pos_ws24_tag
+
+#@ initialdir = $(home)/pos-lab/A2.4
+#@ output = $(home)/job24-np7.out
+#@ error = $(home)/job24-np7.err
+
+#@ notification=always
+#@ notify_user=denys.sobchyshak@tum.de
+#@ queue
+
+#system setup
+perf_off
+
+#minimum optimization objective command
+echo "[INFO] Statistics collection on 7 processes"
+echo "[INFO] Running base code"
+mpiexec -n 7 ./code/gccg ./code/data/pent.geo.bin nodal oneread
+mpiexec -n 7 ./code/gccg ./code/data/pent.geo.bin nodal oneread
+mpiexec -n 7 ./code/gccg ./code/data/pent.geo.bin nodal oneread

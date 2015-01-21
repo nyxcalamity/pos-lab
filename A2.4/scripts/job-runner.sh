68c0f6b269516fb95971356491dfe61bbe962146
Denys S <denys.sobchyshak@gmail.com>
2015-01-21 16:30:39 +0100

    a24:added scripts to perform statistics collection for performance analysis tasks

---
+++ A2.4/scripts/job-runner.sh
@@ -0,0 +1,45 @@
+#/bin/sh
+# Submits all statistics collection jobs to the LoadLeveler
+
+#save directories
+SCRIPT_DIR=$(dirname $0)
+BASE_DIR=$SCRIPT_DIR/..
+WDIR=$(pwd)
+
+#store code location
+CODE=$1
+if [ -z "$CODE" ]; then
+    CODE=$BASE_DIR/code
+else 
+    CODE=$BASE_DIR/$CODE
+fi
+
+#make the code
+cd $CODE
+make
+
+#navigate back and clean up stats
+cd $WDIR
+rm -rf $BASE_DIR/stats/*
+
+#generate job files
+NUM_PROC=(2 3 4 5 6 7 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64)
+for i in ${NUM_PROC[@]}; do
+    $SCRIPT_DIR/job-generator.sh $i $CODE $SCRIPT_DIR
+done
+
+#submit all jobs
+for i in ${NUM_PROC[@]}; do 
+    llsubmit $SCRIPT_DIR/a24-np$i.sh
+done
+
+#wait until jobs are finished
+while [ ! -f $BASE_DIR/stats/job24-np${NUM_PROC[20]}.out ]
+do
+  sleep 5
+done
+
+#pause for a bit and collect all the output
+sleep 5
+cat $BASE_DIR/stats/*.err >> $BASE_DIR/stats/all.err
+cat $BASE_DIR/stats/*.out >> $BASE_DIR/stats/all.out
